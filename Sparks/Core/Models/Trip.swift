27b7fee5bc7cb714cf2a3235a69b53d4317921fc
Irakli Vashakidze <irakli.vashakidze@toptal.com>
2021-12-15 12:11:14 +0400

    User id

--- Sparks/Core/Models/Trip.swift
+++ Sparks/Core/Models/Trip.swift
@@ -32,9 +32,11 @@ class Trip: BaseModelObject {
              purpose,
              community,
              plan,
-             user
+             user,
+             userId
     }
     
+    @objc dynamic private(set) var userId: String?
     @objc dynamic private(set) var city: String?
     @objc dynamic private(set) var purpose: Int = PurposeEnum.leisure.rawValue
     @objc dynamic private(set) var startDate: Int64 = 0
@@ -57,6 +59,7 @@ class Trip: BaseModelObject {
         self.purpose = try container.decodeIfPresent(Int.self, forKey: .purpose) ?? PurposeEnum.leisure.rawValue
         self.community = try container.decodeIfPresent(Int.self, forKey: .community) ?? TripCommunityEnum.alone.rawValue
         self.plan = try container.decodeIfPresent(String.self, forKey: .plan)
+        self.userId = try container.decodeIfPresent(String.self, forKey: .userId)
         self.user = try container.decodeIfPresent(User.self, forKey: .user)
     }
     
